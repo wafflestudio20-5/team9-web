57fcfe84d8c1394910b6366158680196b245513d
aylee2008 <aylee2008@naver.com>
2022-12-15 22:09:52 +0900

    style: Add formatting codes

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'react/prop-types': 'off',
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+    },
+};
