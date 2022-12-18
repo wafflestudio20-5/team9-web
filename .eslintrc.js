d587aed9fa44bda40a2b650b000a594b6ebd16cc
Chansol Lim <lchans0319@gmail.com>
2022-12-19 00:32:27 +0900

    style: Restore .eslintrc.js partially

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,50 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript',
-    'plugin:import/recommended',
-  ],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['import'],
-  rules: {
-    'react/prop-types': 'off',
-    'no-unused-vars': 'off',
-    'no-undef': 'off',
-    'import/order': [
-      'error',
-      {
-        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
-        pathGroups: [
-          {
-            pattern: 'react',
-            group: 'external',
-            position: 'before',
-          },
-          {
-            pattern: 'next',
-            group: 'external',
-          },
-        ],
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-        'newlines-between': 'always',
-      },
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:import/typescript',
+        'plugin:import/recommended',
     ],
-  },
-  settings: {
-    'import/parsers': { '@typescript-eslint/parser': ['.ts'] },
-  },
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    parser: '@typescript-eslint/parser',
+    plugins: ['react', 'import'],
+    rules: {
+        'react/prop-types': 'off',
+        'no-unused-vars': 'off',
+        'no-undef': 'off',
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+                pathGroups: [
+                    {
+                        pattern: 'react',
+                        group: 'external',
+                        position: 'before',
+                    },
+                    {
+                        pattern: 'next',
+                        group: 'external',
+                    },
+                ],
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+                'newlines-between': 'always',
+            },
+        ],
+    },
+    settings: {
+        'import/parsers': { '@typescript-eslint/parser': ['.ts'] },
+    },
 };
