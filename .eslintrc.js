9f7d479c13fedb3b4b21bde4f8e451e71529ae6c
Chansol Lim <lchans0319@gmail.com>
2022-12-19 00:26:28 +0900

    style: Add eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,49 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-    },
-    plugins: ['react'],
-    rules: {
-        'react/prop-types': 'off',
-        'no-unused-vars': 'off',
-        'no-undef': 'off',
-    },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
+    'plugin:import/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['import'],
+  rules: {
+    'react/prop-types': 'off',
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: 'next',
+            group: 'external',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': { '@typescript-eslint/parser': ['.ts'] },
+  },
 };
