a1aa40a335ecd118235d42300ddb57266dd54b42
Chansol Lim <lchans0319@gmail.com>
2022-12-20 00:36:48 +0900

    feat: Add day to DateContext

--- contexts/DateContext.tsx
+++ contexts/DateContext.tsx
@@ -9,15 +9,18 @@ type DateContextData = {
     year: number;
     month: number;
     date: number;
+    day: number;
     setYear(yyyy: number): void;
     setMonth(mm: number): void;
     setDate(dd: number): void;
+    setDay(d: number): void;
 };
 
 const DateContext = createContext<DateContextData>({
     year: 0,
     month: 0,
     date: 0,
+    day: 0,
     setYear() {
         throw new Error('DateContext not provided');
     },
@@ -25,6 +28,9 @@ const DateContext = createContext<DateContextData>({
         throw new Error('DateContext not provided');
     },
     setDate() {
+        throw new Error('DateContext not provided');
+    },
+    setDay() {
         throw new Error('DateContext not provided');
     },
 });
@@ -36,10 +42,20 @@ function DateProvider({ children }: PropsWithChildren) {
     const [year, setYear] = useState(now.getFullYear());
     const [month, setMonth] = useState(now.getMonth() + 1);
     const [date, setDate] = useState(now.getDate());
+    const [day, setDay] = useState(now.getDay());
 
     return (
         <DateContext.Provider
-            value={{ year, month, date, setYear, setMonth, setDate }}
+            value={{
+                year,
+                month,
+                date,
+                day,
+                setYear,
+                setMonth,
+                setDate,
+                setDay,
+            }}
         >
             {children}
         </DateContext.Provider>
