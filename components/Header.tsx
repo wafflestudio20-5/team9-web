133de204381837507be6fcefd6cfd25c1b8ef73a
aylee2008 <aylee2008@naver.com>
2022-12-19 14:16:28 +0900

    style: Format code with prettier

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,101 +11,113 @@ import settings_icon from '../public/images/settings_icon.svg';
 import styles from './Header.module.scss';
 
 const PageOption = {
-  index: { name: 'index', path: '/' },
-  month: { name: 'month', path: '/month' },
-  schedule: { name: 'schedule', path: '/schedule' },
+    index: { name: 'index', path: '/' },
+    month: { name: 'month', path: '/month' },
+    schedule: { name: 'schedule', path: '/schedule' },
 };
 
 const getValueFromPathname = (pathname: string) => {
-  if (pathname === '/') {
-    return '';
-  } else if (pathname === '/month') {
-    return PageOption.month.name;
-  } else if (pathname === '/schedule') {
-    return PageOption.schedule.name;
-  }
+    if (pathname === '/') {
+        return '';
+    } else if (pathname === '/month') {
+        return PageOption.month.name;
+    } else if (pathname === '/schedule') {
+        return PageOption.schedule.name;
+    }
 };
 
 function Header() {
-  const router = useRouter();
+    const router = useRouter();
 
-  const onChangePageOption = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    router.push(PageOption[e.target.value]?.path || '/');
-  };
+    const onChangePageOption = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        router.push(PageOption[e.target.value]?.path || '/');
+    };
 
-  return (
-    <>
-      <header className={styles.header}>
-        {/* sidebar menu */}
-        <div className={styles.sidebarContainer}>
-          <button>
-            <Image src={menu_icon} height={25} alt="sidebar" />
-          </button>
-        </div>
+    return (
+        <>
+            <header className={styles.header}>
+                {/* sidebar menu */}
+                <div className={styles.sidebarContainer}>
+                    <button>
+                        <Image src={menu_icon} height={25} alt="sidebar" />
+                    </button>
+                </div>
 
-        {/* title */}
-        <div className={styles.titleContainer}>
-          <Image src={calendar_icon} height={35} alt="calendar" />
-          <span>캘린더</span>
-        </div>
+                {/* title */}
+                <div className={styles.titleContainer}>
+                    <Image src={calendar_icon} height={35} alt="calendar" />
+                    <span>캘린더</span>
+                </div>
 
-        {/* date (today, year, month) */}
-        <div className={styles.dateContainer}>
-          <div>
-            <button className={styles.today}>오늘</button>
-            <div className={styles.btnContainer}>
-              <button>
-                <Image src={before_icon} height={25} alt="last_month" />
-              </button>
-              <button>
-                <Image src={next_icon} height={25} alt="next_month" />
-              </button>
-            </div>
-            <div className={styles.date}>
-              <button>2023년 12월</button>
-            </div>
-          </div>
-        </div>
+                {/* date (today, year, month) */}
+                <div className={styles.dateContainer}>
+                    <div>
+                        <button className={styles.today}>오늘</button>
+                        <div className={styles.btnContainer}>
+                            <button>
+                                <Image
+                                    src={before_icon}
+                                    height={25}
+                                    alt="last_month"
+                                />
+                            </button>
+                            <button>
+                                <Image
+                                    src={next_icon}
+                                    height={25}
+                                    alt="next_month"
+                                />
+                            </button>
+                        </div>
+                        <div className={styles.date}>
+                            <button>2023년 12월</button>
+                        </div>
+                    </div>
+                </div>
 
-        {/* utils */}
-        <div className={styles.utilContainer}>
-          {/* search */}
-          <div className={styles.search}>
-            <button>
-              <Image src={search_icon} height={25} alt="search" />
-            </button>
-          </div>
-          {/* help */}
-          <div className={styles.help}>
-            <button>
-              <Image src={help_icon} height={25} alt="help" />
-            </button>
-          </div>
-          {/* settings */}
-          <div className={styles.settings}>
-            <button>
-              <Image src={settings_icon} height={25} alt="settings" />
-            </button>
-          </div>
-          {/* page options */}
-          <select
-            name="pageOption"
-            id="pageOption"
-            defaultValue={getValueFromPathname(router.pathname)}
-            onChange={onChangePageOption}
-          >
-            <option value="">캘린더</option>
-            <option value={PageOption.month.name}>월</option>
-            <option value={PageOption.schedule.name}>일정</option>
-          </select>
-          {/* user */}
-          <div className={styles.user}>
-            <button>J</button>
-          </div>
-        </div>
-      </header>
-    </>
-  );
+                {/* utils */}
+                <div className={styles.utilContainer}>
+                    {/* search */}
+                    <div className={styles.search}>
+                        <button>
+                            <Image src={search_icon} height={25} alt="search" />
+                        </button>
+                    </div>
+                    {/* help */}
+                    <div className={styles.help}>
+                        <button>
+                            <Image src={help_icon} height={25} alt="help" />
+                        </button>
+                    </div>
+                    {/* settings */}
+                    <div className={styles.settings}>
+                        <button>
+                            <Image
+                                src={settings_icon}
+                                height={25}
+                                alt="settings"
+                            />
+                        </button>
+                    </div>
+                    {/* page options */}
+                    <select
+                        name="pageOption"
+                        id="pageOption"
+                        defaultValue={getValueFromPathname(router.pathname)}
+                        onChange={onChangePageOption}
+                    >
+                        <option value="">캘린더</option>
+                        <option value={PageOption.month.name}>월</option>
+                        <option value={PageOption.schedule.name}>일정</option>
+                    </select>
+                    {/* user */}
+                    <div className={styles.user}>
+                        <button>J</button>
+                    </div>
+                </div>
+            </header>
+        </>
+    );
 }
 
 export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,10 @@ import type { AppProps } from 'next/app';
 import '../styles/global.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Header />
-      <Component {...pageProps} />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Component {...pageProps} />
+        </>
+    );
 }
