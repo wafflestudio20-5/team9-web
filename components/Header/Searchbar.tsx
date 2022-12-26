2da32df5a99d312430cdd8d52387a0d721869105
Chansol Lim <lchans0319@gmail.com>
2022-12-26 17:53:20 +0900

    feat: Add reset button to searchbar

--- components/Header/Searchbar.tsx
+++ components/Header/Searchbar.tsx
@@ -1,7 +1,14 @@
 import Image from 'next/image';
-import React, { FormEvent, useEffect, useRef } from 'react';
+import React, {
+    ChangeEvent,
+    FormEvent,
+    useEffect,
+    useRef,
+    useState,
+} from 'react';
 
 import { MODAL_NAMES, useModal } from '../../contexts/ModalContext';
+import close_icon from '../../public/images/close_icon.svg';
 import dropdown_icon from '../../public/images/dropdown_icon.svg';
 import search_icon from '../../public/images/search_icon.svg';
 
@@ -15,9 +22,16 @@ interface SearchbarProps {
 export default function Searchbar({ isOpen, close }: SearchbarProps) {
     const { getState, openModal } = useModal();
     const searchRef = useRef<HTMLDivElement>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+    const [query, setQuery] = useState<string>('');
 
     const searchSchedule = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+    };
+
+    const resetQuery = () => {
+        setQuery('');
+        if (inputRef !== null) inputRef.current?.focus();
     };
 
     const onClickOuterArea = (e: MouseEvent) => {
@@ -44,17 +58,27 @@ export default function Searchbar({ isOpen, close }: SearchbarProps) {
         <div className={styles.searchbar} ref={searchRef}>
             <form className={styles.search} onSubmit={searchSchedule}>
                 <button>
-                    <Image src={search_icon} height={25} alt="search" />
+                    <Image src={search_icon} height={24} alt="search" />
                 </button>
-                <input type="text" placeholder="검색" autoFocus />
+                <input
+                    type="search"
+                    value={query}
+                    ref={inputRef}
+                    onChange={e => setQuery(e.target.value)}
+                    placeholder="검색"
+                    autoFocus
+                />
+                <button type="button" onClick={resetQuery}>
+                    <Image src={close_icon} height={24} alt="reset_search" />
+                </button>
                 <button
                     type="button"
                     onClick={() => openModal(MODAL_NAMES.searchDetails)}
                 >
                     <Image
                         src={dropdown_icon}
-                        height={20}
-                        alt="search_details"
+                        height={24}
+                        alt="detailed_search"
                     />
                 </button>
             </form>
