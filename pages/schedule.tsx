31ca309f42aef3e497a62fd8e71eb54cc71ff48e
Chansol Lim <lchans0319@gmail.com>
2022-12-29 01:54:21 +0900

    feat: Add dynamic routing to schedule page

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -1,25 +1,12 @@
 import React from 'react';
 
-import DayinSchedule from '../components/DayinSchedule';
-import Sidebar from '../components/Sidebar';
-import { useSidebarContext } from '../contexts/SidebarContext';
+import { CalendarType } from '../components/Header/CalendarTypeDropDown';
+import RedirectPage from '../components/RedirectPage';
 
-import styles from './schedule.module.scss';
-
-export default function SchedulePage() {
-    const { isOpen } = useSidebarContext();
-    const scheduleData = [
-        { date: 2, day: 5, events: [] },
-        { date: 5, day: 1, events: [] },
-    ];
+export default function RedirectToSchedulePage() {
     return (
-        <div className={styles.wrapper}>
-            {isOpen && <Sidebar />}
-            <div className={styles.scheduleHolder}>
-                {scheduleData.map((dayData, index) => {
-                    return <DayinSchedule key={index} dayData={dayData} />;
-                })}
-            </div>
-        </div>
+        <RedirectPage calendarType={CalendarType.schedule}>
+            일정 페이지로 이동 중
+        </RedirectPage>
     );
 }
