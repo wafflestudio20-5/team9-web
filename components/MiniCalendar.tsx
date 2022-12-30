0619db5e8761f7b85694710298dd24547a522dce
Hyojeong Yoon <emilyyoon3107@gmail.com>
2022-12-31 01:38:44 +0900

    feat: create MiniCalendar structure

--- components/MiniCalendar.tsx
+++ components/MiniCalendar.tsx
@@ -1,3 +1,49 @@
+import Image from 'next/image';
+import { useState, useMemo } from 'react';
+import { useDateContext } from '../contexts/DateContext';
+import before_icon from '../public/images/before_icon.svg';
+import next_icon from '../public/images/next_icon.svg';
+
 export default function MiniCalendar() {
-    return <div>Calendar Component!</div>;
+    const { yearNow, monthNow, dateNow, dayNow } = useDateContext();
+    const [yearToShow, setYearToShow] = useState(yearNow);
+    const [monthToShow, setMonthToShow] = useState(monthNow);
+    const monthData = useMemo(() => {}, [yearToShow, monthToShow]);
+    return (
+        <div>
+            <div>{`${yearNow}년 ${monthNow}월`}</div>
+            <div>
+                <button
+                    onClick={() => {
+                        if (monthToShow == 1) {
+                            setMonthToShow(12);
+                            setYearToShow(yearToShow - 1);
+                        } else {
+                            setMonthToShow(monthToShow - 1);
+                        }
+                    }}
+                >
+                    <Image
+                        src={before_icon}
+                        alt="이전"
+                        width={24}
+                        height={24}
+                    />
+                </button>
+                <button
+                    onClick={() => {
+                        if (monthToShow == 12) {
+                            setYearToShow(yearToShow + 1);
+                            setMonthToShow(1);
+                        } else {
+                            setMonthToShow(monthToShow + 1);
+                        }
+                    }}
+                >
+                    <Image src={next_icon} alt="이후" width={24} height={24} />
+                </button>
+            </div>
+            <div></div>
+        </div>
+    );
 }
