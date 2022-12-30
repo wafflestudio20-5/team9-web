165e58c8541eafb70973abea20e206f4b3344d9f
Chansol Lim <lchans0319@gmail.com>
2022-12-30 22:18:39 +0900

    feat: Add week calendar and day calendar common codes

--- components/DayCalendar.tsx
+++ components/DayCalendar.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+
+import { CalendarType } from '@components/Header/CalendarTypeDropDown';
+import { useDateContext } from '@contexts/DateContext';
 
 export default function DayCalendar() {
+    const { yearNow, monthNow, dateNow } = useDateContext();
+    const router = useRouter();
+    const now = new Date();
+
+    useEffect(() => {
+        if (
+            isNaN(Number(yearNow)) ||
+            isNaN(Number(monthNow)) ||
+            isNaN(Number(dateNow))
+        ) {
+            // redirect to NOT_FOUND page?
+            // or throw 404 error?
+        }
+
+        if (
+            yearNow === now.getFullYear() &&
+            monthNow === now.getMonth() + 1 &&
+            dateNow === now.getDate()
+        ) {
+            router.push(`/${CalendarType.day}/today`);
+        } else {
+            router.push(
+                `/${CalendarType.day}/${yearNow}/${monthNow}/${dateNow}`,
+            );
+        }
+    }, [yearNow, monthNow, dateNow]);
+
     return <div>Day Calendar</div>;
 }
