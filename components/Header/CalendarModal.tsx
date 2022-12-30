0e01b69f3ea33060df0e9f491d901e20d9462984
Hyojeong Yoon <emilyyoon3107@gmail.com>
2022-12-30 14:13:31 +0900

    feat: create MiniCalendar.tsx

--- components/Header/CalendarModal.tsx
+++ components/Header/CalendarModal.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { MODAL_NAMES } from '../../contexts/ModalContext';
+import MiniCalendar from '../MiniCalendar';
 import ModalFrame from '../ModalFrame';
 
 import styles from './CalendarModal.module.scss';
@@ -8,7 +9,9 @@ import styles from './CalendarModal.module.scss';
 export default function CalendarModal() {
     return (
         <ModalFrame modalName={MODAL_NAMES.calendar}>
-            <div className={styles.calendarModal}>calendar component</div>
+            <div className={styles.calendarModal}>
+                <MiniCalendar />
+            </div>
         </ModalFrame>
     );
 }
---
+++ components/MiniCalendar.tsx
@@ -0,0 +1,3 @@
+export default function MiniCalendar() {
+    return <div>Calendar Component!</div>;
+}
