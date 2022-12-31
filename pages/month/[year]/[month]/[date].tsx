747b6b63d48181afe4d0457f605b2f3a605d85c2
Chansol Lim <lchans0319@gmail.com>
2022-12-31 17:51:53 +0900

    fix: Fix month page dynamic routing

--- pages/month/[year]/[month]/[date].tsx
+++ pages/month/[year]/[month]/[date].tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
 
 import MonthCalendar from '@components/MonthCalendar';
+import { CalendarType, useDateContext } from '@contexts/DateContext';
 
 export default function MonthPage() {
+    const { setYearNow, setMonthNow, setDateNow, setDayNow, setCalendarType } =
+        useDateContext();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (!router.isReady) return;
+        const { year, month, date } = router.query;
+
+        if (
+            isNaN(Number(year)) ||
+            isNaN(Number(month)) ||
+            isNaN(Number(date))
+        ) {
+            // redirect to NOT_FOUND page?
+            // or throw 404 error?
+            router.push('/month/today');
+            return;
+        }
+        setYearNow(Number(year));
+        setMonthNow(Number(month));
+        setDateNow(Number(date));
+        setDayNow(new Date(`${year}-${month}-${date}`).getDay());
+        setCalendarType(CalendarType.month);
+    }, [router.isReady, router.query]);
+
     return <MonthCalendar />;
 }
--- pages/month/today.tsx
+++ pages/month/today.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import MonthCalendar from '@components/MonthCalendar';
+import { CalendarType, useDateContext } from '@contexts/DateContext';
 
 export default function MonthTodayPage() {
+    const { setYearNow, setMonthNow, setDateNow, setDayNow, setCalendarType } =
+        useDateContext();
+    const now = new Date();
+
+    useEffect(() => {
+        setYearNow(now.getFullYear());
+        setMonthNow(now.getMonth() + 1);
+        setDateNow(now.getDate());
+        setDayNow(now.getDay());
+        setCalendarType(CalendarType.month);
+    }, [now]);
+
     return <MonthCalendar />;
 }
