42f7574ecc055ea1fb769e1be5859d5d2a46fe26
Chansol Lim <lchans0319@gmail.com>
2023-01-01 17:11:56 +0900

    refactor: Use absolute path for _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
 
-import Header from '../components/Header/Header';
-import DateProvider from '../contexts/DateContext';
-import ModalProvider, { ModalContainer } from '../contexts/ModalContext';
-import SessionProvider from '../contexts/SessionContext';
-import SidebarProvider from '../contexts/SidebarContext';
-import '../styles/global.scss';
+import Header from '@components/Header/Header';
+import DateProvider from '@contexts/DateContext';
+import ModalProvider, { ModalContainer } from '@contexts/ModalContext';
+import SessionProvider from '@contexts/SessionContext';
+import SidebarProvider from '@contexts/SidebarContext';
+import '@styles/global.scss';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     return (
