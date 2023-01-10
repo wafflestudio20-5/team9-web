a141de94dbbb3a3cd703a56c7846571915fb3fe0
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-01-11 02:25:19 +0900

    refactor: Separate AddFriendsDropDown to independent and reusable UserSearchDropDown component

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -3,11 +3,12 @@ import React from 'react';
 import styles from './Sidebar.module.scss';
 
 import MiniCalendar from '@components/MiniCalendar';
-import { AddFriendsDropDown } from './AddFriendsDropDown';
+import { UserSearchDropDown } from '@components/UserSearchDropDown';
 import { useSidebarContext } from '@contexts/SidebarContext';
 
 export default function Sidebar() {
     const { isClosing } = useSidebarContext();
+    const sendFollowRequest = () => {};
     return (
         <div className={isClosing ? `${styles.closing}` : `${styles.open}`}>
             <div className={styles.wrapper}>
@@ -16,7 +17,7 @@ export default function Sidebar() {
                     <MiniCalendar />
                 </div>
                 <div className={styles.addFriends}>
-                    <AddFriendsDropDown />
+                    <UserSearchDropDown toExecute={sendFollowRequest} />
                 </div>
                 <div>My Calenders</div>
                 <div>Other Calenders</div>
--- components/UserSearchDropDown.tsx
+++ components/UserSearchDropDown.tsx
@@ -5,7 +5,7 @@ import {
     useDropDown,
 } from '@components/DropDown';
 import { useState, useRef, useEffect } from 'react';
-import styles from './AddFriendsDropDown.module.scss';
+import styles from './UserSearchDropDown.module.scss';
 import axios from 'axios';
 import account_default_icon from '@images/account_default_icon.svg';
 import close_icon from '@images/close_icon.svg';
@@ -18,7 +18,15 @@ interface UserData {
     email: string;
 }
 
-export function AddFriendsDropDown() {
+interface UserSearchDropDownProps {
+    toExecute: Function;
+    width?: string; // custom width when used elsewhere
+}
+
+export function UserSearchDropDown({
+    toExecute,
+    width,
+}: UserSearchDropDownProps) {
     const { dropDownRef, openDropDown, closeDropDown, isOpen } = useDropDown();
     const { stored, setStored } = useLocalStorage<UserData[] | null>(
         'searchRecord_addFriend',
@@ -27,14 +35,33 @@ export function AddFriendsDropDown() {
     const [searchInput, setSearchInput] = useState('');
     const [selectedResults, setSelectedResults] = useState<UserData[]>([]);
     const [suggestions, setSuggestions] = useState<UserData[] | null>(
-        stored ? stored : null,
-    ); // for showing the most recent 4 search records
-    // Specific implementation method is subject to discussion. Options include:
-    // 1) GET Api when the AddFriendsDropDown component first mounts
-    // 2) Save to localStorage every time a search happens -> get data from localStorage when component mounts
+        stored ? stored : null, // handle undefined
+    );
     const containerRef = useRef<HTMLDivElement | null>(null);
     // useRef to place suggestions dropdown directly below searchbox
     // needed b/c searchbox height changes according to the lenght of selectedResults
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        if (!isOpen) {
+            openDropDown(); // to tackle case of continuous search wihtout clicking again
+        }
+        setSearchInput(e.target.value);
+        if (e.target.value !== '') {
+            axios
+                .get(
+                    `http://ec2-43-201-9-194.ap-northeast-2.compute.amazonaws.com/api/v1/social/search/candidate/?search=${e.target.value}`,
+                )
+                .then(res =>
+                    res.data.results.length !== 0
+                        ? setSuggestions(res.data.results)
+                        : setSuggestions(null),
+                )
+                .catch(() => setSuggestions(null));
+        } else {
+            setSuggestions(stored ? stored : null);
+        }
+    };
 
     const handleSubmit = (item: UserData | null) => {
         console.log(item);
@@ -64,7 +91,12 @@ export function AddFriendsDropDown() {
     return (
         <DropDown dropDownRef={dropDownRef}>
             <DropDownHeader openDropDown={() => {}} style={{ display: 'flex' }}>
-                <div className={styles.container} ref={containerRef}>
+                {/* openDropDown does nothing here to prevent dropdown expanding when removing items from selectedResults */}
+                <div
+                    className={styles.container}
+                    ref={containerRef}
+                    style={{ width: width }}
+                >
                     {selectedResults.map((item, index) => {
                         return (
                             <div key={index} className={styles.selected}>
@@ -94,29 +126,11 @@ export function AddFriendsDropDown() {
                         <input
                             value={searchInput}
                             onChange={e => {
-                                e.preventDefault();
-                                if (!isOpen) {
-                                    openDropDown();
-                                }
-                                setSearchInput(e.target.value);
-                                if (e.target.value !== '') {
-                                    axios
-                                        .get(
-                                            `http://ec2-43-201-9-194.ap-northeast-2.compute.amazonaws.com/api/v1/social/search/candidate/?search=${e.target.value}`,
-                                        )
-                                        .then(res =>
-                                            res.data.results.length !== 0
-                                                ? setSuggestions(
-                                                      res.data.results,
-                                                  )
-                                                : setSuggestions(null),
-                                        )
-                                        .catch(() => setSuggestions(null));
-                                } else {
-                                    setSuggestions(stored ? stored : null);
-                                }
+                                handleChange(e);
                             }}
                             onClick={openDropDown}
+                            // openDropDown only called when clicking the input area
+                            // (excludes instances of clicking selectedResults items)
                             className={styles.input}
                         />
                     </form>
@@ -142,7 +156,7 @@ export function AddFriendsDropDown() {
                             );
                         })
                     ) : (
-                        <li>No results</li>
+                        <li>No results...</li>
                     )}
                 </ul>
             </DropDownBody>
