0f15593ca05f6ecdba21ce026fc158c44b633c5a
Chansol Lim <lchans0319@gmail.com>
2023-01-10 20:08:58 +0900

    fix: Format time

--- components/TimeDropDown.tsx
+++ components/TimeDropDown.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 
 import {
     DropDown,
@@ -6,50 +6,69 @@ import {
     DropDownHeader,
     useDropDown,
 } from '@components/DropDown';
+import { formatTime } from '@utils/formatTime';
 
-const getTimeList = () => {
-    const hours = Array(24)
-        .fill(0)
-        .map((v, i) => i);
-    const minutes = Array(4)
-        .fill(0)
-        .map((v, i) => i * 15);
-    const times: number[][] = [];
-
-    hours.forEach(h => {
-        minutes.forEach(m => {
-            times.push([h, m]);
-        });
-    });
-
-    return times;
-};
-
-function Time({ hour, min }: { hour: number; min: number }) {
-    return (
-        <li>
-            {hour}시 {min}분
-        </li>
-    );
+interface Time {
+    hour: number;
+    minute: number;
 }
 
-export default function TimeDropDown({ title }: { title: string }) {
+interface TimeDropDownProps {
+    title: string;
+    time: Time;
+    setTime: Dispatch<SetStateAction<Time>>;
+}
+
+export default function TimeDropDown({
+    title,
+    time,
+    setTime,
+}: TimeDropDownProps) {
     const { dropDownRef, isOpen, openDropDown, closeDropDown } = useDropDown();
+
+    const getTimeList = () => {
+        const hours = Array(24)
+            .fill(0)
+            .map((v, i) => i);
+        const minutes = Array(4)
+            .fill(0)
+            .map((v, i) => i * 15);
+        const times: number[][] = [];
+
+        hours.forEach(h => {
+            minutes.forEach(m => {
+                times.push([h, m]);
+            });
+        });
+
+        return times;
+    };
 
     return (
         <DropDown dropDownRef={dropDownRef}>
             <DropDownHeader openDropDown={openDropDown}>
-                <input type="text" placeholder={title} onClick={openDropDown} />
+                <input
+                    type="text"
+                    value={formatTime(time.hour, time.minute, false)}
+                    onClick={openDropDown}
+                    placeholder={title}
+                />
             </DropDownHeader>
             <DropDownBody
                 isOpen={isOpen}
                 style={{ height: '150px', overflow: 'auto' }}
             >
                 <ul style={{ width: '100px' }}>
-                    {getTimeList().map((time, i) => {
+                    {getTimeList().map((t, i) => {
                         return (
-                            <li key={i}>
-                                {time[0]}시 {time[1]}분
+                            <li
+                                key={i}
+                                onClick={() => {
+                                    setTime({ hour: t[0], minute: t[1] });
+                                    closeDropDown();
+                                }}
+                            >
+                                {formatTime(t[0], t[1], false)}
                             </li>
                         );
                     })}
