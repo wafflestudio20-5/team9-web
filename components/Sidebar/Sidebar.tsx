c2555b479c279ae4ea744def854dfd715d0e21cc
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-01-10 14:17:44 +0900

    design: Add soft expand and collapse animation to sidebar

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -4,19 +4,23 @@ import styles from './Sidebar.module.scss';
 
 import MiniCalendar from '@components/MiniCalendar';
 import { AddFriendsDropDown } from './AddFriendsDropDown';
+import { useSidebarContext } from '@contexts/SidebarContext';
 
 export default function Sidebar() {
+    const { isClosing } = useSidebarContext();
     return (
-        <div className={styles.wrapper}>
-            <div>Create Button</div>
-            <div className={styles.calendar}>
-                <MiniCalendar />
+        <div className={isClosing ? `${styles.closing}` : `${styles.open}`}>
+            <div className={styles.wrapper}>
+                <div>Create Button</div>
+                <div className={styles.calendar}>
+                    <MiniCalendar />
+                </div>
+                <div className={styles.addFriends}>
+                    <AddFriendsDropDown />
+                </div>
+                <div>My Calenders</div>
+                <div>Other Calenders</div>
             </div>
-            <div className={styles.addFriends}>
-                <AddFriendsDropDown />
-            </div>
-            <div>My Calenders</div>
-            <div>Other Calenders</div>
         </div>
     );
 }
