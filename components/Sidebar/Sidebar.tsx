69a2f62327a052579bdddacfeb2811db13a09931
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-01-11 22:52:15 +0900

    fix: Add authorization header to sendFollowRequest

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -7,13 +7,16 @@ import { UserSearchDropDown } from '@components/UserSearchDropDown';
 import { useSidebarContext } from '@contexts/SidebarContext';
 import { Accordion } from '@components/Accordion';
 import { mapCalendarToggle } from './calendarToggle';
+import { useSessionContext } from '@contexts/SessionContext';
 
 export default function Sidebar() {
     const { isClosing } = useSidebarContext();
+    const { accessToken } = useSessionContext();
     const sendFollowRequest = (item: UserDataForSearch) => {
         axios.post(
             'http://ec2-43-201-9-194.ap-northeast-2.compute.amazonaws.com/api/v1/social/network/',
             { followee: { pk: item.pk } },
+            { headers: { Authorization: `Bearer ${accessToken}` } },
         );
     };
     return (
