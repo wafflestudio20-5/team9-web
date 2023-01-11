a45a1c492c33485d89d56a98602d79a9af579721
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-01-12 03:44:16 +0900

    feat: Create ColoredCheckbox component

---
+++ components/ColoredCheckbox.tsx
@@ -0,0 +1,51 @@
+import React, { ReactNode, useState, useEffect } from 'react';
+import styles from './ColoredCheckbox.module.scss';
+import white_check_icon from '@images/white_check_icon.svg';
+import Image from 'next/image';
+
+interface CustomCheckboxProps {
+    state: boolean;
+    setState: React.Dispatch<React.SetStateAction<boolean>>;
+    color: string;
+    size: { size: number; unit: string };
+    children?: ReactNode;
+}
+
+function DefaultChild() {
+    return <Image src={white_check_icon} alt="O" className={styles.check} />;
+}
+
+export default function ColoredCheckbox({
+    state,
+    setState,
+    color,
+    size,
+    children,
+}: CustomCheckboxProps) {
+    return (
+        <div
+            className={styles.backdrop}
+            style={{
+                height: `${size.size * 1.7}${size.unit}`,
+                width: `${size.size * 1.7}${size.unit}`,
+            }}
+        >
+            <div
+                onClick={() => setState(!state)}
+                className={
+                    state
+                        ? `${styles.box} ${styles.checked}`
+                        : `${styles.box} ${styles.default}`
+                }
+                style={{
+                    height: `${size.size}${size.unit}`,
+                    width: `${size.size}${size.unit}`,
+                    borderColor: color,
+                    backgroundColor: state ? color : 'white',
+                }}
+            >
+                {children ? children : <DefaultChild />}
+            </div>
+        </div>
+    );
+}
--- components/Sidebar/calendarToggle.tsx
+++ components/Sidebar/calendarToggle.tsx
@@ -1,10 +1,25 @@
+import ColoredCheckbox from '@components/ColoredCheckbox';
+import styles from './calendarToggle.module.scss';
+import React, { useState } from 'react';
+
 interface CalendarDataForToggle {
     pk: number;
     name: string;
 }
 
 export default function CalendarToggle({ name }: { name: string }) {
-    return <div>{name}</div>;
+    const [isChosen, setIsChosen] = useState(false);
+    return (
+        <div className={styles.wrapper}>
+            <ColoredCheckbox
+                state={isChosen}
+                setState={setIsChosen}
+                size={{ size: 20, unit: 'px' }}
+                color="#1967cd"
+            />
+            <div className={styles.text}>{name}</div>
+        </div>
+    );
 }
 
 export const mapCalendarToggle = (data: CalendarDataForToggle) => {
