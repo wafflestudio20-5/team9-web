40cdd54ebe9ebd2536b6e51cd06fdeba2b7677b9
Chansol Lim <lchans0319@gmail.com>
2023-01-12 02:16:56 +0900

    refactor: Refactor codes related to time

--- lib/utils/formatTime.ts
+++ lib/utils/formatTime.ts
@@ -1,17 +1,12 @@
-export const formatTime = (hour: number, minute: number, is24Hour: boolean) => {
-    if (is24Hour) {
-        return `${String(hour).padStart(2, '0')}:${String(minute).padStart(
-            2,
-            '0',
-        )}:00`;
-    }
-
+export const formatTime = (time: Date) => {
     let half;
+    let hour = time.getHours();
+    const minute = time.getMinutes();
     if (hour < 12) {
         if (hour === 0) hour = 12;
         half = '오전';
     } else {
-        hour -= 12;
+        if (hour !== 12) hour -= 12;
         half = '오후';
     }
     return `${half} ${hour}:${String(minute).padStart(2, '0')}`;
