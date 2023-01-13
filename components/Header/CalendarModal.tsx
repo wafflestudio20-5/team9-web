b3747db0ff8af336d42bac12a18adc3ca6aaac2e
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-01-13 19:28:23 +0900

    refac: Refactor MiniCalendar in Header/CalendarModal.tsx

--- components/Header/CalendarModal.tsx
+++ components/Header/CalendarModal.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useMemo } from 'react';
 
 import styles from './CalendarModal.module.scss';
 
+import { useCalendarContext } from '@contexts/CalendarContext';
 import MiniCalendar from '@components/MiniCalendar';
 import ModalFrame from '@components/ModalFrame';
 import { MODAL_NAMES } from '@contexts/ModalContext';
 
 export default function CalendarModal() {
+    const router = useRouter();
+    const { year, month, date } = router.query;
+    const { calendarType } = useCalendarContext();
+    const dateObjNow = useMemo(() => {
+        return year
+            ? new Date(Number(year), Number(month) - 1, Number(date))
+            : new Date();
+    }, [year, month, date]);
     return (
         <ModalFrame modalName={MODAL_NAMES.calendar}>
             <div className={styles.calendarModal}>
-                <MiniCalendar />
+                <MiniCalendar
+                    dateVariable={dateObjNow}
+                    onDateClickFunction={date => {
+                        router.push(
+                            `/${calendarType}/${date?.getFullYear()}/${
+                                date.getMonth() + 1
+                            }/${date.getDate()}`,
+                        );
+                    }}
+                />
             </div>
         </ModalFrame>
     );
