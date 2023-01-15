c7a55259b8166e5197a2fe3d0ce96e00f17fe047
aylee2008 <aylee2008@naver.com>
2023-01-15 20:02:18 +0900

    feat: Amend day component for reusability

--- components/DayCalendar/DayCalendar.tsx
+++ components/DayCalendar/DayCalendar.tsx
@@ -4,18 +4,16 @@ import styles from './DayCalendar.module.scss';
 
 import DayComponent from '@components/DayComponent';
 import Sidebar from '@components/Sidebar/Sidebar';
+import { useDateContext } from '@contexts/DateContext';
 import { useSidebarContext } from '@contexts/SidebarContext';
 
 export default function DayCalendar() {
     const { isOpen } = useSidebarContext();
-
+    const { dateNow, dayNow } = useDateContext();
     return (
         <div className={styles.wrapper}>
             {isOpen ? <Sidebar /> : <></>}
-            {/* <div className={styles.timeHolder}>
-
-            </div> */}
-            <DayComponent />
+            <DayComponent isToday={true} date={dateNow} day={dayNow} />
         </div>
     );
 }
--- components/DayComponent.tsx
+++ components/DayComponent.tsx
@@ -2,23 +2,48 @@ import React from 'react';
 
 import styles from './DayComponent.module.scss';
 
-import { useDateContext } from '@contexts/DateContext';
+import { useCalendarContext } from '@contexts/CalendarContext';
 import { DAYS_ARR } from '@utils/formatDay';
 
-export default function DayComponent() {
-    const { dateNow, dayNow } = useDateContext();
-
+export default function DayComponent({
+    isToday,
+    date,
+    day,
+}: {
+    isToday: boolean;
+    date: number;
+    day: number;
+}) {
+    const { calendarType } = useCalendarContext();
     const twentyFourHours: Array<number> = Array.from(
         { length: 24 },
         (v, i) => i + 1,
     );
 
+    const getHeadType = () => {
+        if (calendarType === 'month') {
+            return styles.headRowInWeek;
+        } else if (calendarType === 'day') {
+            return styles.headRowInDay;
+        }
+    };
+
+    // get type of date
+    // draw blue circle when date is 'today'
+    const getDateType = () => {
+        if (isToday) {
+            return styles.dayTodayNumber;
+        } else {
+            return styles.dayNotTodayNumber;
+        }
+    };
+
     return (
         <div className={styles.dayContentHolder}>
-            <div className={styles.headRowInDay}>
+            <div className={getHeadType()}>
                 <div className={styles.dayHolder}>
-                    <div className={styles.dayText}>{DAYS_ARR[dayNow]}</div>
-                    <div className={styles.dayNumber}>{dateNow}</div>
+                    <div className={styles.dayText}>{DAYS_ARR[day]}</div>
+                    <div className={getDateType()}>{date}</div>
                 </div>
             </div>
             <div className={styles.mainSchedules}>
