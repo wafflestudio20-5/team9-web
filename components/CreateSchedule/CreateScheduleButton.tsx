305c4e9dbe7f505b9e5f1fc81f433215f90e7129
Chansol Lim <lchans0319@gmail.com>
2023-01-15 22:21:25 +0900

    Make CreateScheduleButton customizable

--- components/CreateSchedule/CreateScheduleButton.tsx
+++ components/CreateSchedule/CreateScheduleButton.tsx
@@ -6,15 +6,25 @@ import styles from './CreateScheduleButton.module.scss';
 import { MODAL_NAMES, useModal } from '@contexts/ModalContext';
 import add_icon from '@images/add_icon.svg';
 
-export default function CreateScheduleButton() {
+interface CreateScheduleButtonProps {
+    text?: string;
+    style?: React.CSSProperties;
+}
+
+export default function CreateScheduleButton({
+    text,
+    style,
+}: CreateScheduleButtonProps) {
     const { openModal } = useModal();
 
     return (
         <div
             className={styles.createScheduleButton}
             onClick={() => openModal(MODAL_NAMES.createSchedule)}
+            style={style}
         >
             <Image src={add_icon} alt="create_schedule" width={40} />
+            {text && <span>{text}</span>}
         </div>
     );
 }
