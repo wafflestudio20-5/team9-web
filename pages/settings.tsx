e4c9aeeb77ef96be4ac7c94ec3026676ff6e06ad
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-01-20 04:06:01 +0900

    feat: Settings page basic structure

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,14 +1,34 @@
+import { Accordion } from '@components/Accordion';
 import { useThemeContext } from '@contexts/ThemeContext';
 import styles from './settings.module.scss';
 
+const settings = { 일반: ['테마', '기타'] };
+
+const SettingItem = ({ text }: { text: string }) => {
+    return <div>{text}</div>;
+};
+
 export default function SettingsPage() {
-    const { theme, setTheme } = useThemeContext();
+    const { setTheme } = useThemeContext();
 
     return (
-        <>
-            <button onClick={() => setTheme('light')}>light</button>
-            <button onClick={() => setTheme('dark')}>dark</button>
-            <div className={styles.tester}>background color</div>
-        </>
+        <div className={styles.container}>
+            <div className={styles.sidebar}>
+                <Accordion
+                    title="일반"
+                    sequence={settings.일반}
+                    mapFunction={(item, index) => {
+                        return <SettingItem key={index} text={item} />;
+                    }}
+                />
+            </div>
+            <div className={styles.main}>
+                <div className={styles.settingBlock}>
+                    <div className={styles.title}>테마</div>
+                    <button onClick={() => setTheme('light')}>light</button>
+                    <button onClick={() => setTheme('dark')}>dark</button>
+                </div>
+            </div>
+        </div>
     );
 }
