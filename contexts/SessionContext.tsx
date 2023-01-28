0f7d2b73ef8f5997e2cf4b4ec8c18210c2a7ee33
aylee2008 <aylee2008@naver.com>
2023-01-28 18:33:23 +0900

    feat: Remove unnecessary imports and format code

--- contexts/SessionContext.tsx
+++ contexts/SessionContext.tsx
@@ -10,7 +10,7 @@ import React, {
 } from 'react';
 import Swal from 'sweetalert2';
 
-import keys from '../secrets.json';
+//import keys from '../secrets.json';
 
 import { apiStagingEndPoint } from '@apis/endpoint';
 
