b75f38378cd688252c343d561ebad54b1f62092d
aylee2008 <aylee2008@naver.com>
2023-01-29 21:12:22 +0900

    feat: Use param store

--- contexts/SessionContext.tsx
+++ contexts/SessionContext.tsx
@@ -13,6 +13,7 @@ import Swal from 'sweetalert2';
 //import keys from '../secrets.json';
 
 import { apiStagingEndPoint } from '@apis/endpoint';
+import { getEncryptedParameter } from '@utils/paramStore';
 
 interface LoginInfo {
     email: string;
@@ -94,11 +95,12 @@ const REACT_APP_KAKAO_REST_API_KEY =
     process.env.REACT_APP_KAKAO_REST_API_KEY || '';
 //const { REACT_APP_GOOGLE_CLIENT_ID, REACT_APP_KAKAO_REST_API_KEY } = keys;
 
-console.log(process.env);
-console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
-console.log(process.env.REACT_APP_KAKAO_REST_API_KEY);
-console.log(process.env.AWS_REGION);
-console.log(process.env.NODE_ENV);
+// console.log(process.env.ENVIRONMENT_VARIABLE);
+// console.log(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+// console.log(process.env.REACT_APP_KAKAO_REST_API_KEY);
+// console.log(process.env.AWS_REGION);
+// console.log(process.env.NODE_ENV);
+console.log(process.env.AWS_SDK_LOAD_CONFIG);
 
 export const useSessionContext = () => useContext(SessionContext);
 
@@ -209,9 +211,14 @@ export default function SessionProvider({ children }: PropsWithChildren) {
     };
 
     // referenced https://www.hacksoft.io/blog/google-oauth2-with-django-react-part-2
-    const openGoogleLoginPage = useCallback(() => {
+    const openGoogleLoginPage = useCallback(async () => {
         const googleAuthUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
         const redirectUri = 'api/v1/user/login/google/callback/';
+
+        const REACT_APP_GOOGLE_CLIENT_ID =
+            (await getEncryptedParameter(
+                '/frontend/dearj/react_app_google_client_id',
+            )) || '';
 
         const scope = [
             'https://www.googleapis.com/auth/userinfo.email',
@@ -232,9 +239,14 @@ export default function SessionProvider({ children }: PropsWithChildren) {
         window.location.href = `${googleAuthUrl}?${urlParams}`;
     }, []);
 
-    const openKakaoLoginPage = useCallback(() => {
+    const openKakaoLoginPage = useCallback(async () => {
         const kakaoAuthUrl = 'https://kauth.kakao.com/oauth/authorize';
         const redirectUri = 'api/v1/user/login/kakao/callback/';
+
+        const REACT_APP_KAKAO_REST_API_KEY =
+            (await getEncryptedParameter(
+                '/frontend/dearj/react_app_kakao_rest_api_key',
+            )) || '';
 
         const params = {
             response_type: 'code',
