58c9dac7ae3c2f54c6f6349150ba7a123f2424af
aylee2008 <aylee2008@naver.com>
2023-01-30 14:19:12 +0900

    chore: Add log for debugging

--- contexts/SessionContext.tsx
+++ contexts/SessionContext.tsx
@@ -257,6 +257,8 @@ export default function SessionProvider({ children }: PropsWithChildren) {
                     },
                 })
                 .then(response => {
+                    console.log(postSocialLoginData.accessToken);
+                    console.log(postSocialLoginData.refreshToken);
                     setUser({
                         pk: response.data.pk,
                         email: response.data.email,
