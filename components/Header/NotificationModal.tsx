67e0b8556d80668610bd0b6d2bd2bcfcf1872290
aylee2008 <aylee2008@naver.com>
2023-02-01 18:52:59 +0900

    feat: Add notification-getting functions in notification api

--- components/Header/NotificationModal.tsx
+++ components/Header/NotificationModal.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import styles from './NotificationModal.module.scss';
 
+import { getFollowRequests, getScheduleRequests } from '@apis/notification';
 import ModalFrame from '@components/ModalFrame';
 import { MODAL_NAMES, useModal } from '@contexts/ModalContext';
+import { useSessionContext } from '@contexts/SessionContext';
 
 export default function NotificationModal() {
+    const { accessToken } = useSessionContext();
+    const getAndSetFollowRequests = () => {
+        getFollowRequests(accessToken)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error.message);
+            });
+    };
+    const getAndSetScheduleRequests = () => {
+        getScheduleRequests(accessToken)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error.message);
+            });
+    };
+
+    useEffect(() => {
+        getAndSetFollowRequests();
+        getAndSetScheduleRequests();
+    });
+
     return (
         <ModalFrame modalName={MODAL_NAMES.notification}>
             <div className={styles.notificationModal}></div>
--- lib/apis/notification.ts
+++ lib/apis/notification.ts
@@ -2,6 +2,12 @@ import axios from 'axios';
 
 import { apiEndPoint } from './endpoint';
 
-export const getFollowRequests = () => {};
+export const getFollowRequests = (accessToken: string | null) =>
+    axios.get(`${apiEndPoint}/social/network/notification`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+    });
 
-export const getScheduleRequests = () => {};
+export const getScheduleRequests = (accessToken: string | null) =>
+    axios.get(`${apiEndPoint}/calendar/schedule/notification`, {
+        headers: { Authorization: `Bearer ${accessToken}` },
+    });
