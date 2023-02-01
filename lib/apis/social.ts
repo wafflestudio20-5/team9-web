a90cda962bda2f2a30ef66486f200c37f14233d0
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-02-01 22:22:20 +0900

    fix: Fix follow request API

--- lib/apis/social.ts
+++ lib/apis/social.ts
@@ -4,7 +4,7 @@ import { apiEndPoint } from './endpoint';
 
 export const followRequestAPI = (pk: number, accessToken: string | null) =>
     axios.post(
-        `${apiEndPoint}/social/network/`,
+        `${apiEndPoint}/social/network/followee/`,
         { followee: { pk: pk } },
         { headers: { Authorization: `Bearer ${accessToken}` } },
     );
