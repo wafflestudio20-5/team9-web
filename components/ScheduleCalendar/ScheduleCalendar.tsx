a670b7014414035d6d3f1134f5fae66937478e19
Hyojeong Yoon <emilyyoon3107@gmail.com>
2023-02-03 19:22:42 +0900

    fix: Fix event ordering in ScheduleCalendar

--- components/ScheduleCalendar/ScheduleCalendar.tsx
+++ components/ScheduleCalendar/ScheduleCalendar.tsx
@@ -1,12 +1,17 @@
 import axios from 'axios';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, {
+    useEffect,
+    useMemo,
+    useRef,
+    useState,
+    useCallback,
+} from 'react';
 
 import styles from './ScheduleCalendar.module.scss';
 
 import { DayinSchedule } from '@components/ScheduleCalendar/DayinSchedule';
 import Sidebar from '@components/Sidebar/Sidebar';
 import { useSidebarContext } from '@contexts/SidebarContext';
-import useInfiniteScroll from '@hooks/useInfiniteScroll';
 import {
     NumberedEvent,
     NumberedEventsByDay,
@@ -18,6 +23,7 @@ import { getEntireScheduleAPI } from '@apis/calendar';
 import { useSessionContext } from '@contexts/SessionContext';
 import { useRouter } from 'next/router';
 import CreateScheduleButton from '@components/ScheduleModal/CreateScheduleButton';
+import { useCalendarContext } from '@contexts/CalendarContext';
 
 //temporary until backend changes
 function toHttps(url: string) {
@@ -31,6 +37,7 @@ function toHttps(url: string) {
 export default function ScheduleCalendar() {
     const { isOpen } = useSidebarContext();
     const { accessToken, user } = useSessionContext();
+    const { needUpdate } = useCalendarContext();
     const router = useRouter();
     const { year, month, date } = router.query;
     const today = new Date();
@@ -48,148 +55,83 @@ export default function ScheduleCalendar() {
                   today.getDate(),
               );
     }, [year, month, date]);
-    const [scheduleEvent, setScheduleEvent] = useState<NumberedEvent[]>();
-    const [scheduleEventByDay, setScheduleEventByDay] =
+    const [next, setNext] = useState('');
+    const [scheduleEvent, setScheduleEvent] = useState<FullSchedule[]>();
+    const [scheduleEventByDay, setScheduleEventByday] =
         useState<NumberedEventsByDay>();
-    const [bufferData, setBufferData] = useState<NumberedEvent[]>();
+    const [loadMore, setLoadMore] = useState(false);
+    const [isEndOfList, setIsEndOfList] = useState(false);
+    const targetRef = useRef<HTMLDivElement>(null);
 
-    const scrollHolderRef = useRef<HTMLDivElement>(null);
-    const targetRef = React.createRef<HTMLDivElement>();
-    const [next, setNext] = useState<string>('');
     useEffect(() => {
-        console.log('double call');
-        // initial API call -> get total of 40 entries, only display 20
-        if (user?.pk) {
-            getEntireScheduleAPI(
-                {
-                    pk: user?.pk,
-                    from: formatDate(dateObj),
-                    to: formatDate(dateBoundaryObj),
-                },
-                accessToken,
-            )
-                .then(res => {
-                    setScheduleEvent(
-                        res.data.results.map(
-                            (event: FullSchedule, index: number) => {
-                                return { num: index, event: event };
-                            },
-                        ),
-                    );
-                    return res.data.next;
-                })
-                .then(url => {
-                    if (url === null) {
-                        return;
-                    } else {
-                        axios
-                            .get(toHttps(url), {
-                                headers: {
-                                    Authorization: `Bearer ${accessToken}`,
-                                },
-                            })
-                            .then(res => {
-                                setNext(toHttps(res.data.next));
-                                setBufferData(
-                                    res.data.results &&
-                                        res.data.results.map(
-                                            (
-                                                event: FullSchedule,
-                                                index: number,
-                                            ) => {
-                                                return {
-                                                    num: 20 + index,
-                                                    event: event,
-                                                };
-                                            },
-                                        ),
-                                );
-                            });
-                    }
-                });
+        // initial api call
+        if (!user || needUpdate) {
+            return;
         }
-    }, [user?.pk, dateObj]);
+        getEntireScheduleAPI(
+            {
+                pk: user?.pk,
+                from: formatDate(dateObj),
+                to: formatDate(dateBoundaryObj),
+            },
+            accessToken,
+        ).then(res => {
+            setNext(res.data.next);
+            setScheduleEvent(res.data.results);
+        });
+    }, [user, needUpdate]);
 
-    const [setTarget] = useInfiniteScroll(
-        (entry: IntersectionObserverEntry, observer: IntersectionObserver) => {
-            if (entry.isIntersecting) {
-                console.log(entry);
-                if (scheduleEvent && next) {
-                    if (next) {
-                        axios
-                            .get(toHttps(next), {
-                                headers: {
-                                    Authorization: `Bearer ${accessToken}`,
-                                },
-                            })
-                            .then(res => {
-                                if (res.data.next) {
-                                    setNext(toHttps(res.data.next));
-                                }
-                                setNext(toHttps(res.data.next));
-                                if (bufferData) {
-                                    setScheduleEvent([
-                                        ...scheduleEvent,
-                                        ...bufferData,
-                                    ]);
-                                    setBufferData([]);
-                                }
-                                setBufferData(res.data.results);
-                            });
-                    }
-                }
-                observer.unobserve(entry.target);
-            }
-        },
-        { threshold: [0, 1], root: scrollHolderRef?.current },
-    );
-
-    useEffect(() => {
-        if (typeof targetRef !== 'function' && targetRef.current) {
-            setTarget(targetRef.current);
+    const fetchData = useCallback(async () => {
+        if (!next) {
+            setIsEndOfList(true);
         }
-    }, [scheduleEventByDay]);
-
-    const refIndex = useMemo(() => {
-        return scheduleEventByDay
-            ? Object.keys(scheduleEventByDay).length - 1
-            : -1;
-    }, [scheduleEventByDay]);
+        axios
+            .get(next, {
+                headers: { Authorization: `Bearer ${accessToken}` },
+            })
+            .then(res => {
+                setNext(res.data.next);
+                setScheduleEvent(
+                    scheduleEvent
+                        ? [...res.data.results, ...scheduleEvent]
+                        : res.data.results,
+                );
+            });
+    }, []);
 
     useEffect(() => {
-        console.log('recalc');
+        // layer events after data has been fetched
         if (scheduleEvent) {
-            const eventsByDay = {} as NumberedEventsByDay;
-            const { frozenEvents, scrollableEvents } = getFrozenEvents(
-                scheduleEvent.map(data => {
-                    return data.event;
-                }),
-            );
-            if (frozenEvents) {
-                frozenEvents.forEach(event => {
-                    const dateObj = new Date(event.start_at);
-                    while (isDateIncluded(dateObj, event)) {
-                        const dateString = formatDate(dateObj);
-                        if (eventsByDay[dateString]) {
-                            eventsByDay[dateString].push({
-                                num: 0,
-                                event: event,
-                            });
-                        } else {
-                            eventsByDay[dateString] = [
-                                {
-                                    num: 0,
-                                    event: event,
-                                },
-                            ];
-                        }
-                        dateObj.setDate(dateObj.getDate() + 1);
-                    }
-                });
+            console.log(getScheduleEventsByDay(scheduleEvent));
+            setScheduleEventByday(getScheduleEventsByDay(scheduleEvent));
+        }
+    }, [scheduleEvent]);
+
+    useEffect(() => {
+        if (!targetRef.current || isEndOfList) return;
+
+        // load more on intersection
+        const io = new IntersectionObserver((entries, observer) => {
+            if (entries[0].isIntersecting) {
+                fetchData();
             }
-            if (scrollableEvents) {
-                scrollableEvents.forEach(event => {
-                    const dateString = event.start_at.split(' ')[0];
+        });
+        io.observe(targetRef.current);
+
+        return () => {
+            io.disconnect();
+        };
+    }, [fetch, isEndOfList]);
+
+    const getScheduleEventsByDay = (scheduleEvent: FullSchedule[]) => {
+        const eventsByDay = {} as NumberedEventsByDay;
+        const { frozenEvents, scrollableEvents } =
+            getFrozenEvents(scheduleEvent);
+        if (frozenEvents) {
+            frozenEvents.forEach(event => {
+                const dateObj = new Date(event.start_at);
+                while (isDateIncluded(dateObj, event)) {
+                    const dateString = formatDate(dateObj);
                     if (eventsByDay[dateString]) {
                         eventsByDay[dateString].push({
                             num: 0,
@@ -203,49 +145,70 @@ export default function ScheduleCalendar() {
                             },
                         ];
                     }
-                });
-            }
-            const sorted = Object.entries(eventsByDay).sort(([key1], [key2]) =>
-                key1 < key2 ? -1 : 1,
-            );
-            let i = 0;
-            sorted.forEach(([dateString, eventData], index) => {
-                eventsByDay[dateString] = eventsByDay[dateString].map(
-                    (eventData, index) => {
-                        return { num: index + i, event: eventData.event };
-                    },
-                );
-                i += eventData?.length;
+                    dateObj.setDate(dateObj.getDate() + 1);
+                }
             });
-            setScheduleEventByDay(eventsByDay);
         }
-    }, [scheduleEvent]);
+        if (scrollableEvents) {
+            scrollableEvents.forEach(event => {
+                const dateString = event.start_at.split(' ')[0];
+                if (eventsByDay[dateString]) {
+                    eventsByDay[dateString].push({
+                        num: 0,
+                        event: event,
+                    });
+                } else {
+                    eventsByDay[dateString] = [
+                        {
+                            num: 0,
+                            event: event,
+                        },
+                    ];
+                }
+            });
+        }
+
+        const sorted = Object.entries(eventsByDay).sort(
+            ([key1, item1], [key2, item2]) => (key1 < key2 ? -1 : 1),
+        );
+        let i = 0;
+        sorted.forEach(([dateString, eventData], index) => {
+            eventsByDay[dateString] = eventsByDay[dateString].map(
+                (eventData, index) => {
+                    return { num: index + i, event: eventData.event };
+                },
+            );
+            i += eventData?.length;
+        });
+        return eventsByDay;
+    };
 
     return (
         <div className={styles.wrapper}>
             {isOpen ? <Sidebar /> : <CreateScheduleButton />}
-            <div className={styles.scrollHolder} ref={scrollHolderRef}>
+            <div className={styles.scrollHolder}>
                 <div className={styles.scrollContent}>
                     {scheduleEventByDay &&
-                        Object.entries(scheduleEventByDay).map(
-                            ([dateString, eventData], index) => {
+                        Object.entries(scheduleEventByDay)
+                            .sort(([key1, item1], [key2, item2]) =>
+                                key1 < key2 ? -1 : 1,
+                            )
+                            .map(([dateString, eventData], index) => {
                                 return (
                                     <DayinSchedule
                                         key={index}
                                         dateString={dateString}
                                         eventData={eventData}
-                                        ref={
-                                            index ===
-                                            Object.keys(scheduleEventByDay)
-                                                .length -
-                                                1
-                                                ? targetRef
-                                                : undefined
-                                        }
                                     />
                                 );
-                            },
-                        )}
+                            })}
+
+                    <div
+                        style={{ position: 'absolute', bottom: '100px' }}
+                        ref={targetRef}
+                    >
+                        {isEndOfList && <div>end of schedules</div>}
+                    </div>
                 </div>
             </div>
         </div>
