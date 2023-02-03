c0ca494d12399d48149ecc90a96b03a2484a6f19
Chansol Lim <lchans0319@gmail.com>
2023-02-03 22:32:09 +0900

    feat: Add mainpage dark theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,6 +33,7 @@ export default function MainPage() {
                         </button>
                     )}
                 </div>
+                <div className={styles.image}></div>
             </div>
         </div>
     );
