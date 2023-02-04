ceaeb189528aed64645db27c2a1474ed23cff9e4
Chansol Lim <lchans0319@gmail.com>
2023-02-04 12:37:54 +0900

    feat: fix: Fix schedules_json type

--- pages/blog/post/create.tsx
+++ pages/blog/post/create.tsx
@@ -1,55 +1,26 @@
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 import styles from './create.module.scss';
 
 import { createPostAPI } from '@apis/blog';
 import PostEditor from '@components/Blog/PostEditor';
 import ScheduleList from '@components/Blog/ScheduleList';
-import { useSessionContext } from '@contexts/SessionContext';
-import { FullSchedule } from '@customTypes/ScheduleTypes';
-import { errorToast, successToast } from '@utils/customAlert';
 import { useScheduleContext } from '@contexts/ScheduleContext';
-
-// temp data for related schedule list
-const schedulesData: FullSchedule[] = [
-    {
-        id: 993,
-        title: '',
-        created_at: '',
-        updated_at: '',
-        created_by: 0,
-        is_recurring: false,
-        description: '',
-        participants: [],
-        show_content: false,
-        protection_level: 1,
-        recurring_schedule_group: null,
-        recurring_end_at: null,
-        cron_expr: null,
-        start_at: '',
-        end_at: '',
-    },
-];
+import { useSessionContext } from '@contexts/SessionContext';
+import { errorToast, successToast } from '@utils/customAlert';
 
 export default function PostCreatePage() {
     const { accessToken } = useSessionContext();
-    const [schedules, setSchedules] = useState(schedulesData); // need to bring related schedules from local storage
-    const {
-        setIsSelectMode,
-        schedules: localStorageSchedules,
-        setSchedules: setLocalStorageSchedules,
-    } = useScheduleContext();
+    const { schedules, setSchedules } = useScheduleContext();
     const router = useRouter();
 
     const getScheduleIds = () => {
         if (!schedules) {
             return [];
         }
-        return schedules.map(event => {
-            return event.id;
-        });
+        return schedules.map(event => ({ pk: event.id }));
     };
 
     const createPost = async (newPost: FormData) => {
@@ -58,7 +29,7 @@ export default function PostCreatePage() {
             const res = await createPostAPI(newPost, accessToken);
             successToast('새 글을 생성했습니다.');
             router.push(`/blog/post/${res.data.pid}`);
-            setLocalStorageSchedules([]);
+            setSchedules([]);
         } catch (error) {
             const message = '글을 생성하지 못했습니다.';
             if (axios.isAxiosError(error)) {
@@ -74,7 +45,7 @@ export default function PostCreatePage() {
     };
 
     useEffect(() => {
-        setSchedules(localStorageSchedules ?? []);
+        setSchedules(schedules ?? []);
     }, []);
 
     return (
